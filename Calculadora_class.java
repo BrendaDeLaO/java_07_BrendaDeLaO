23242e279918fba0bd46f018cf3bf23de4c472c6
Brenda De La O Florindez <brenda.delao.florindez@vallegrande.edu.pe>
2024-09-02 10:22:00 -0500

    Add files via upload

---
+++ Calculadora_class.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package JFrame_Calculadora;
+
+/**
+ *
+ * @author Santa Ana
+ */
+public class Calculadora_class {
+    
+    public String sumar(String a, String b){
+        int resultado = Integer.parseInt(a)+ Integer.parseInt(b);
+        return String.valueOf(resultado);
+    }
+    
+    public String restar(String a, String b){
+        int resultado = Integer.parseInt(a)- Integer.parseInt(b);
+        return String.valueOf(resultado);
+    }
+    
+    public String multiplicar(String a, String b){
+        int resultado = Integer.parseInt(a)* Integer.parseInt(b);
+        return String.valueOf(resultado);
+    }
+    
+    public String dividir(String a, String b){
+        double resultado = Double.parseDouble(a)/ Double.parseDouble(b);
+        return String.valueOf(resultado);
+    }
+}
